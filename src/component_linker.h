30093f6c7ba397fbf85bb99f41fc59bd8a356004
Nathan Ross Powell <nathanrosspowell@gmail.com>
2015-08-15 21:44:12 -0400

    Don't need to use the type here

--- src/component_linker.h
+++ src/component_linker.h
@@ -21,7 +21,7 @@ namespace minmod
                 assert(m_currentlyLinking != INVALID_ID);
                 auto& ownedPairs = m_entryMap[COMPONENT::GetStaticId()];
                 ownedPairs[m_currentlyLinking] = std::make_pair(
-                    [add](Interface* ptr)
+                    [add](auto ptr)
                     {
                         assert(dynamic_cast<COMPONENT*>(ptr) != nullptr);
                         add(static_cast<COMPONENT*>(ptr));
