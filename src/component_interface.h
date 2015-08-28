318d237ba0469dd8a5577da487326d3abcbb1688
Nathan Ross Powell <nathanrosspowell@gmail.com>
2015-08-28 00:04:47 -0400

    First pass of real documentation
    Looking into using https://github.com/jessevdk/cldoc so I've started
    with that documentation standard.

--- src/component_interface.h
+++ src/component_interface.h
@@ -13,25 +13,54 @@ namespace minmod
     namespace component
     {
         class Linker;
+
+        /* The interface to the component::Manager.
+         *
+         * Any class which wants to work in the coponent system has to implement this pure interface.
+         * The only public functions are for infering the type.
+         * The rest of the functions are private with access via friend classes only.
+         */
         class Interface : public Serializer
         {
-        public:
-            // Id functions.
+        public: // Id functions.
+            /* Get a unique <Id> for this class type 
+             *
+             * The <Id> has to be unique per type, not per instance.
+             * It is adviced to use something like a CRC32 of the value used in <GetName>.
+             *
+             * @return A unique <Id> for this class.
+             */
             virtual Id GetId() const = 0;
+            /* Get a unique <Name> for this class type 
+             *
+             * The <Name> has to be unique per type, not per instance.
+             * It is adviced to use something that matches the class name.
+             * E.g. use "Position" for a `PositionComponent`.
+             *
+             * @return A unique <Name> for this class.
+             */
             virtual Name GetName() const = 0;
 
-        private:
+        private: // Linker interface.
             friend class Linker;
-            // Where to define dependencies on other componentMap, may be called
-            // multiple times.
+            /* Where to define dependencies on other componentMap, may be called
+             * multiple times.
+             */
             virtual void MakeLinks(Linker& linker) = 0;
 
-        private:
+        private: // Manager interface.
             friend class Manager;
-            // Constructor and destructor replacements. Only ever calle once.
+            // Constructor replacement. Only ever calle once.
             virtual void Create() = 0;
+            // Destuctor replacement. Only ever calle once.
             virtual void Destroy() = 0;
         };
+
+        /* A unique pointer to an interface.
+         *
+         * The manager will store one unique pointer for the lifetime of the component.
+         * There will only be limited access to it and raw pointers to it should not be cached.
+         */
         using UniquePtr = std::unique_ptr<Interface>;
     }
 }
