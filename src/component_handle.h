b9954068ee42d3e38125791d9bd800aed4cce0f6
Nathan Ross Powell <nathanrosspowell@gmail.com>
2015-08-27 20:56:09 -0400

    Split up handle class into inline file

--- src/component_handle.h
+++ src/component_handle.h
@@ -19,30 +19,13 @@ namespace minmod
             class Adapter
             {
             public:
-                explicit Adapter(T* ptr) : m_ptr(ptr), m_checked(false)
-                {
-                }
-                explicit operator bool() const
-                {
-                    assert(m_checked == false);
-                    m_checked = true;
-                    return m_ptr != nullptr;
-                }
-                T& Get()
-                {
-                    assert(m_checked == true);
-                    assert(m_ptr != nullptr);
-                    return *m_ptr;
-                }
-                const T& Get() const
-                {
-                    assert(m_checked == true);
-                    assert(m_ptr != nullptr);
-                    return *m_ptr;
-                }
+                explicit Adapter(T* ptr);
+                explicit operator bool() const;
+                T& Get();
+                const T& Get() const;
 
             private:
-                T* m_ptr;
+                T* const m_ptr;
                 mutable bool m_checked;
             };
 
@@ -51,36 +34,16 @@ namespace minmod
             Handle(Manager& manager, OwnerId ownerId, Id id);
 
         public:
-            Adapter Get();
-            void Do(std::function<void(T&)> func);
+            Adapter Get() const;
+            void Do(std::function<void(T&)> func) const;
 
         private:
             Manager& m_manager;
-            OwnerId m_ownerId;
-            Id m_componentId;
+            const OwnerId m_ownerId;
+            const Id m_componentId;
         };
-
-        template <class T> Handle<T>::Handle(Manager& manager, OwnerId ownerId) : m_manager(manager), m_ownerId(ownerId), m_componentId(T::GetStaticId())
-        {
-        }
-
-        template <class T> Handle<T>::Handle(Manager& manager, OwnerId ownerId, Id id) : m_manager(manager), m_ownerId(ownerId), m_componentId(id)
-        {
-        }
-
-        template <class T> typename Handle<T>::Adapter Handle<T>::Get()
-        {
-            auto ptr = m_manager.Get(m_ownerId, m_componentId);
-            assert(dynamic_cast<T*>(ptr) != nullptr);
-            return std::move(Adapter(static_cast<T*>(ptr)));
-        }
-
-        template <class T> void Handle<T>::Do(std::function<void(T&)> func)
-        {
-            if (auto adapter = Get())
-            {
-                func(adapter.Get());
-            }
-        }
     }
 }
+
+// Inline file with template definitions.
+#include "component_handle.inl"
---
+++ src/component_handle.inl
@@ -0,0 +1,52 @@
+namespace minmod
+{
+    namespace component
+    {
+        template <class T> Handle<T>::Adapter::Adapter(T* ptr) : m_ptr(ptr), m_checked(false)
+        {
+        }
+
+        template <class T> Handle<T>::Adapter::operator bool() const
+        {
+            assert(m_checked == false);
+            m_checked = true;
+            return m_ptr != nullptr;
+        }
+        
+        template <class T> T& Handle<T>::Adapter::Get()
+        {
+            assert(m_checked == true);
+            assert(m_ptr != nullptr);
+            return *m_ptr;
+        }
+
+        template <class T> const T& Handle<T>::Adapter::Get() const
+        {
+            assert(m_checked == true);
+            assert(m_ptr != nullptr);
+            return *m_ptr;
+        }
+
+        template <class T> Handle<T>::Handle(Manager& manager, OwnerId ownerId) : m_manager(manager), m_ownerId(ownerId), m_componentId(T::GetStaticId())
+        {
+        }
+
+        template <class T> Handle<T>::Handle(Manager& manager, OwnerId ownerId, Id id) : m_manager(manager), m_ownerId(ownerId), m_componentId(id)
+        {
+        }
+
+        {
+            auto ptr = m_manager.Get(m_ownerId, m_componentId);
+            assert(dynamic_cast<T*>(ptr) != nullptr);
+            return std::move(Adapter(static_cast<T*>(ptr)));
+        }
+
+        {
+            if (auto adapter = Get())
+            {
+                func(adapter.Get());
+            }
+        }
+    }
+}
+
