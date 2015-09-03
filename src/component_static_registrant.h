ade0a951e76dfda76b906ff01d6f84277f1cd125
Nathan Ross Powell <nathanrosspowell@gmail.com>
2015-09-02 21:21:31 -0400

    Static registration header comments

--- src/component_static_registrant.h
+++ src/component_static_registrant.h
@@ -8,15 +8,29 @@ namespace minmod
 {
     namespace component
     {
+        /* Helper class for statically registering a class in the <Factory>.
+         *
+         * The template class needs to inherit from <Interface> and also implement:
+         *     1) <GetStaticId> for the <Id>
+         *     2) <GetStaticName> for the <Name>
+         */
         template <class COMPONENT> class StaticRegistrant
         {
         public:
+            /* Constructor.
+             *
+             * Auto adds the component to the factory using the template information.
+             */
             StaticRegistrant()
             {
                 TRACE("Insert component: " << COMPONENT::GetStaticName());
                 Factory::GetInstance().Insert<COMPONENT>();
             }
 
+            /* Destructor.
+             *
+             * Auto removes the component to the factory using the template information.
+             */
             ~StaticRegistrant()
             {
                 TRACE("Erase component: " << COMPONENT::GetStaticName());
