03dffdc4878d13ace284de2bd3c9851d142f2dd1
Nathan Ross Powell <nathanrosspowell@gmail.com>
2015-09-03 12:22:15 -0400

    Update travis_build.sh

--- travis_build.sh
+++ travis_build.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 # Debug build and run.
-make debug || exit 1
-./minmod_component || exit 1
+pushd src
+make debug
+./minmod_component
 # Release build and run.
-make || exit 1
-./minmod_component || exit 1
+make
+./minmod_component
+popd
