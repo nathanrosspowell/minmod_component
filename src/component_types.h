61963659388917187de832be7a1bbfed4435b0b4
Nathan Ross Powell <nathanrosspowell@gmail.com>
2015-09-07 17:10:04 -0400

    More comments

--- src/component_types.h
+++ src/component_types.h
@@ -1,12 +1,28 @@
 #pragma once
 
+/* <minmod> : Minimal Modern C++ Libs.
+ *
+ * Minimal: What I decided as the basic needs for the system and nothing more.
+ * Modern: Using features from C++11/14/17 when it makes sense, not just for the sake of it.
+ */
 namespace minmod
 {
+    /* Component namespace
+     *
+     * All component related code is under this namespace so the naming can be kept short.
+     */
     namespace component
     {
+        // The owner of a component.
         using OwnerId = std::int32_t;
+
+        // The fast identifier for a component.
         using Id = std::uint32_t;
+
+        // The slow identifier for a component.
         using Name = std::string;
+
+        // The test for an invalid <Id>, returned in place of 'null'.
         static const Id INVALID_ID = 0;
     }
 }
